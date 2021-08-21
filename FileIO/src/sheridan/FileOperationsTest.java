6e7becd71041e51c7f2f0d513dc5bf27afb69864
Ravi <raviramchand8@gmail.com>
2021-08-21 18:27:37 -0400

    called writeFile() and readFile()

--- FileIO/src/sheridan/FileOperationsTest.java
+++ FileIO/src/sheridan/FileOperationsTest.java
@@ -1,9 +1,30 @@
 package sheridan;
 
+import java.util.Scanner;
+
 public class FileOperationsTest {
 	public static void main(String []args) {
+		Scanner input = new Scanner(System.in); 
+		String firstName;
+		String lastName; 
+		int id;
 		
+		System.out.print("Enter your first name: ");
+		firstName = input.next();
 		
+		System.out.print("Enter your last name: ");
+		lastName = input.next();
+		
+		System.out.print("Enter your id: ");
+		id = input.nextInt();
+		
+		FileOperations fo = new FileOperations(firstName, lastName, id);
+		
+		fo.writeFile();
+		
+		fo.readFile();
+		
+		input.close();
 		
 		//End of main method
 	}
