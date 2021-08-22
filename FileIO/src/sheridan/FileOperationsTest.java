72dbb4fa076d08712aa3b5d035441e257109f6ea
Ravi <raviramchand8@gmail.com>
2021-08-22 18:00:27 -0400

    added a while loop

--- FileIO/src/sheridan/FileOperationsTest.java
+++ FileIO/src/sheridan/FileOperationsTest.java
@@ -7,27 +7,36 @@ public class FileOperationsTest {
 		Scanner input = new Scanner(System.in); 
 		String firstName;
 		String lastName; 
-		int id;
+		int id;	
+		boolean again = true;
+
+		while(again) {
+			System.out.print("Enter your first name: ");
+			firstName = input.next();
+
+			System.out.print("Enter your last name: ");
+			lastName = input.next();
+
+			System.out.print("Enter your id: ");
+			id = input.nextInt();
+
+			FileOperations fo = new FileOperations(firstName, lastName, id);
+			fo.writeFile();
+			
+			System.out.print("Would you like to create another user? (true = yes/false = no): ");
+			again = input.nextBoolean();
+			
+			System.out.println("Your file entries: \n");
+			fo.readFile();			
+		}
 		
-		System.out.print("Enter your first name: ");
-		firstName = input.next();
-		
-		System.out.print("Enter your last name: ");
-		lastName = input.next();
-		
-		System.out.print("Enter your id: ");
-		id = input.nextInt();
-		
-		FileOperations fo = new FileOperations(firstName, lastName, id);
-		
-		fo.writeFile();
-		
-		fo.readFile();
+		System.exit(1);
 		
 		input.close();
-		
+
 		//End of main method
 	}
-	
+
+
 	//End of FileOperationsTest class
 }
