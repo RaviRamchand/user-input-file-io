ae38b75faad52aea3fd04c061af0b412ce42855f
Ravi <raviramchand8@gmail.com>
2021-08-22 18:27:35 -0400

    added exception handling

--- FileIO/src/sheridan/FileOperationsTest.java
+++ FileIO/src/sheridan/FileOperationsTest.java
@@ -1,5 +1,6 @@
 package sheridan;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class FileOperationsTest {
@@ -11,27 +12,41 @@ public class FileOperationsTest {
 		boolean again = true;
 
 		while(again) {
-			System.out.print("Enter your first name: ");
-			firstName = input.next();
+			try {
+				System.out.print("Enter your first name: ");
+				firstName = input.next();
 
-			System.out.print("Enter your last name: ");
-			lastName = input.next();
+				System.out.print("Enter your last name: ");
+				lastName = input.next();
 
-			System.out.print("Enter your id: ");
-			id = input.nextInt();
+				System.out.print("Enter your id: ");
+				id = input.nextInt();
 
-			FileOperations fo = new FileOperations(firstName, lastName, id);
-			fo.writeFile();
-			
-			System.out.print("Would you like to create another user? (true = yes/false = no): ");
-			again = input.nextBoolean();
-			
-			System.out.println("Your file entries: \n");
-			fo.readFile();			
+				if(id < 0) {
+					throw new IllegalArgumentException("You must enter a number bigger than zero");
+				}
+
+				FileOperations fo = new FileOperations(firstName, lastName, id);
+				fo.writeFile();
+
+				System.out.print("Would you like to create another user? (true = yes/false = no): ");
+				again = input.nextBoolean();
+
+				System.out.println("Your file entries: \n");
+				fo.readFile();	
+
+			}
+			catch(InputMismatchException e) {
+				System.err.println("Invalid entry... try again.");
+				input.next(); 
+			}
+			catch(IllegalArgumentException e) {
+				System.err.println(e.getMessage());
+			}
 		}
-		
+
 		System.exit(1);
-		
+
 		input.close();
 
 		//End of main method
